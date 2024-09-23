3122a00e54a4526b38525f43e8ae580957719ed2
Adam Amusa <Adamamusa2004@gmail.com>
2024-09-23 21:47:17 +0100

    Google and facebook buttons added aswell as sign in button

--- src/Login.js
+++ src/Login.js
@@ -1,14 +1,129 @@
-import { TextField } from "@mui/material";
+import { FormControl, FormLabel, TextField } from "@mui/material";
+import Box from "@mui/material/Box";
+import { styled } from '@mui/material/styles';
+import MuiCard from '@mui/material/Card';
+import Button from '@mui/material/Button';
+import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
+import Divider from "@mui/material/Divider";
+import { GoogleIcon, FacebookIcon, SitemarkIcon } from './CustomIcons';
+
+const Card = styled(MuiCard)(({ theme }) => ({
+    display: 'flex',
+    flexDirection: 'column',
+    alignSelf: 'center',
+    width: '100%',
+    padding: theme.spacing(4),
+    gap: theme.spacing(2),
+    margin: 'auto',
+    [theme.breakpoints.up('sm')]: {
+        maxWidth: '450px',
+    },
+    boxShadow:  
+        'hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px',
+    ...theme.applyStyles('dark', {
+        boxShadow:
+            'hsla(220, 30%, 5%, 0.5) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.08) 0px 15px 35px -5px',
+    }),
+}));
+
 
 function Login() {
     return (
-      <div className="Login">
-        <TextField fullWidth label="Email" id="fullWidth" />
-        <TextField fullWidth label="Password" id="fullWidth" />
+        <div className="Login">
+            <Card variant="outlined" >
+
+                <Box
+                    component="form"
+                    //onSubmit={handleSubmit}
+                    noValidate
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        width: '100%',
+                        gap: 2,
+                    }}
+                >
+                    <FormControl>
+                        <FormLabel htmlFor="email">Email</FormLabel>
+                        <TextField placeholder="Email"
+                            fullWidth id="email"
+                            type="email"
+                            name="email"
+                            autoFocus
+                            required
+                            variant="outlined" />
+                    </FormControl>
 
 
-      </div>
+
+
+                    <FormControl>
+
+                        <FormLabel htmlFor="password">Password</FormLabel>
+                        <TextField
+                            name="password"
+                            placeholder="Password"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                            autoFocus
+                            required
+                            fullWidth
+                            variant="outlined"
+                        />
+
+                    </FormControl>
+
+
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                    //onClick={validateInputs}
+                    >
+                        Sign in
+                    </Button>
+
+                    <Typography sx={{ textAlign: 'center' }}>
+                        Don&apos;t have an account?{' '}
+                        <span>
+                            <Link
+                                href=""
+                                variant="body2"
+                                sx={{ alignSelf: 'center' }}
+                            >
+                                Sign up
+                            </Link>
+                        </span>
+                    </Typography>
+                </Box>
+                <Divider>or</Divider>
+                <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="outlined"
+                        onClick={() => alert('Sign in with Google')}
+                        startIcon={<GoogleIcon />}
+                    >
+                        Sign in with Google
+                    </Button>
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="outlined"
+                        onClick={() => alert('Sign in with Facebook')}
+                        startIcon={<FacebookIcon />}
+                    >
+                        Sign in with Facebook
+                    </Button>
+
+                </Box>
+            </Card>
+
+        </div>
     );
-  }
-  
-  export default Login;
+}
+
+export default Login;
