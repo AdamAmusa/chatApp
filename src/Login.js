4b0718994eedc13fe6c60f9629ce1bcdf19db7e7
Adam Amusa <Adamamusa2004@gmail.com>
2024-09-25 20:21:37 +0100

    Design changes made to sign in page

--- src/Login.js
+++ src/Login.js
@@ -1,6 +1,6 @@
-import { FormControl, FormLabel, TextField } from "@mui/material";
+import { FormControl, FormLabel, TextField, InputLabel, InputBase } from "@mui/material";
 import Box from "@mui/material/Box";
-import { styled } from '@mui/material/styles';
+import { styled,alpha } from '@mui/material/styles';
 import MuiCard from '@mui/material/Card';
 import Button from '@mui/material/Button';
 import Typography from "@mui/material/Typography";
@@ -8,30 +8,112 @@ import Link from "@mui/material/Link";
 import Divider from "@mui/material/Divider";
 import { GoogleIcon, FacebookIcon, SitemarkIcon } from './CustomIcons';
 
+
+
 const Card = styled(MuiCard)(({ theme }) => ({
     display: 'flex',
     flexDirection: 'column',
     alignSelf: 'center',
-    width: '100%',
+    width: '325px',
+    borderRadius: 10,
     padding: theme.spacing(4),
     gap: theme.spacing(2),
     margin: 'auto',
     [theme.breakpoints.up('sm')]: {
-        maxWidth: '450px',
+        maxWidth: '',
     },
-    boxShadow:  
+    boxShadow:
         'hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px',
     ...theme.applyStyles('dark', {
         boxShadow:
             'hsla(220, 30%, 5%, 0.5) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.08) 0px 15px 35px -5px',
     }),
+    
+}));
+
+const BootstrapInput = styled(InputBase)(({ theme }) => ({
+    'label + &': {
+        marginTop: theme.spacing(3),
+    },
+    '& .MuiInputBase-input': {
+        borderRadius: 8,
+        position: 'relative',
+        backgroundColor: '#F3F6F9',
+        border: '1px solid',
+        borderColor: '#E0E3E7',
+        fontSize: 16,
+        width: '100%',
+        padding: '10px 12px',
+        transition: theme.transitions.create([
+            'border-color',
+            'background-color',
+            'box-shadow',
+        ]),
+        // Use the system font instead of the default Roboto font.
+        fontFamily: [
+            '-apple-system',
+            'BlinkMacSystemFont',
+            '"Segoe UI"',
+            'Roboto',
+            '"Helvetica Neue"',
+            'Arial',
+            'sans-serif',
+            '"Apple Color Emoji"',
+            '"Segoe UI Emoji"',
+            '"Segoe UI Symbol"',
+        ].join(','),
+        '&:focus': {
+            boxShadow: `${alpha(theme.palette.primary.main, 0.25)} 0 0 0 0.2rem`,
+            borderColor: theme.palette.primary.main,
+        },
+        ...theme.applyStyles('dark', {
+            backgroundColor: '#1A2027',
+            borderColor: '#2D3843',
+        }),
+    },
+}));
+
+const ButtonS = styled(Button)(({ theme }) => ({
+    borderRadius: 9,
+    fontFamily: [
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        'Roboto',
+        '"Helvetica Neue"',
+        'Arial',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+    ] 
+}));
+
+const ButtonI = styled(Button)(({ theme }) => ({
+    borderRadius: 100,
+    fontSize: 11,
+    color:"black",
+    borderColor:"grey",
+    width: '200px',
+    fontFamily: [
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        'Roboto',
+        '"Helvetica Neue"',
+        'Arial',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+    ]
 }));
 
 
 function Login() {
     return (
         <div className="Login">
-            <Card variant="outlined" >
+            <Card variant="" >
 
                 <Box
                     component="form"
@@ -44,46 +126,31 @@ function Login() {
                         gap: 2,
                     }}
                 >
-                    <FormControl>
-                        <FormLabel htmlFor="email">Email</FormLabel>
-                        <TextField placeholder="Email"
-                            fullWidth id="email"
-                            type="email"
-                            name="email"
-                            autoFocus
-                            required
-                            variant="outlined" />
+                    <FormControl variant="standard">
+                        <InputLabel shrink htmlFor="bootstrap-input">
+                            Email
+                        </InputLabel>
+                        <BootstrapInput id="bootstrap-input" />
                     </FormControl>
 
 
-
-
-                    <FormControl>
-
-                        <FormLabel htmlFor="password">Password</FormLabel>
-                        <TextField
-                            name="password"
-                            placeholder="Password"
-                            type="password"
-                            id="password"
-                            autoComplete="current-password"
-                            autoFocus
-                            required
-                            fullWidth
-                            variant="outlined"
-                        />
-
+                    <FormControl variant="standard">
+                        <InputLabel shrink htmlFor="bootstrap-input">
+                            Password
+                        </InputLabel>
+                        <BootstrapInput id="bootstrap-input" />
                     </FormControl>
 
 
-                    <Button
+                    <ButtonS
                         type="submit"
                         fullWidth
                         variant="contained"
+                        
                     //onClick={validateInputs}
                     >
                         Sign in
-                    </Button>
+                    </ButtonS>
 
                     <Typography sx={{ textAlign: 'center' }}>
                         Don&apos;t have an account?{' '}
@@ -98,26 +165,26 @@ function Login() {
                         </span>
                     </Typography>
                 </Box>
-                <Divider>or</Divider>
-                <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
-                    <Button
+                
+                <Box sx={{ display: 'flex', flexDirection: 'row', gap: 2 }}>
+                    <ButtonI
                         type="submit"
                         fullWidth
                         variant="outlined"
                         onClick={() => alert('Sign in with Google')}
                         startIcon={<GoogleIcon />}
                     >
-                        Sign in with Google
-                    </Button>
-                    <Button
+                        Log in with Google
+                    </ButtonI>
+                    <ButtonI
                         type="submit"
                         fullWidth
                         variant="outlined"
                         onClick={() => alert('Sign in with Facebook')}
                         startIcon={<FacebookIcon />}
                     >
-                        Sign in with Facebook
-                    </Button>
+                        Log in with Facebook
+                    </ButtonI>
 
                 </Box>
             </Card>
