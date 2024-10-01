4c9bb4bf3aa75772e5a1ea50a215f371b1347486
Adam Amusa <Adamamusa2004@gmail.com>
2024-10-01 22:14:34 +0100

    Added log out button but with no functionality

--- src/chatPage.js
+++ src/chatPage.js
@@ -1,7 +1,23 @@
+import { Box } from '@mui/material';
+import Button from '@mui/material/Button';
+
 function ChatPage() {
+    console.log("In chat page");
+  
+    
     return (
+    
         <div>
-            Chat Page !
+            <Box sx={{
+                display: "flex",
+                position: "absolute",
+                top: 0,
+                left: 0,    
+            }}>
+                <Button variant="outlined" size="small">Logout</Button>
+            </Box>
+            
+            <h1 style={{color: "black"}} >Chat Page !</h1>
         </div>
     );
 }
