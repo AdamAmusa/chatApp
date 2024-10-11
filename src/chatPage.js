2f1f3712e5b720b2b434e6d7c54670d774340e01
AdamAmusa <92576185+AdamAmusa@users.noreply.github.com>
2024-10-11 10:49:58 +0000

    Started chatPage logic

--- src/chatPage.js
+++ src/chatPage.js
@@ -1,13 +1,22 @@
 import { Box } from '@mui/material';
 import Button from '@mui/material/Button';
 import { useSignOut } from './server';
+import { collection, orderBy, limit, addDoc} from 'firebase/firestore';
+
 
 function ChatPage() {
     console.log("In chat page");
     const signOut = useSignOut();
-    
+
+    const messageRef = collection("message");
+    const query = messageRef.orderBy('createdAt').limit(25);
+
+    const [messages] = addDoc(query, {idField: 'id'});
+
+
     return (
     
+
         <div>
             <Box sx={{
                 display: "flex",
@@ -18,6 +27,9 @@ function ChatPage() {
                 <Button onClick={signOut} variant="outlined" size="small">Logout</Button>
             </Box>
             
+            
+
+
             <h1 style={{color: "black"}} >Chat Page !</h1>
         </div>
     );
