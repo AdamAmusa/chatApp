e585dbf4e6d31e79018aa21ebd530c99e50475f1
Adam Amusa <Adamamusa2004@gmail.com>
2024-10-16 18:06:34 +0100

    created style class for if the sender and receiver

--- src/chatmessage.js
+++ src/chatmessage.js
@@ -1,8 +1,14 @@
+import { auth } from "./server";
+
 function ChatMessage(props) {
     const { text, uid } = props.message;
 
+    const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
     return (
-        <p>{text}</p>
+        <div className={`message ${messageClass}`}>
+             <p>{text}</p>
+        </div>
+       
     )
 
 }
