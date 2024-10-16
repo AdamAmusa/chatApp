8d3df2b6525a8b84458d6f094481cb9cf0110ec6
Adam Amusa <Adamamusa2004@gmail.com>
2024-10-16 19:15:18 +0100

    Added styling to the input field and button

--- src/chatPage.js
+++ src/chatPage.js
@@ -1,38 +1,34 @@
 import { Box } from '@mui/material';
 import Button from '@mui/material/Button';
 import { useSignOut, db } from './server';
-import { collection, orderBy, limit, query as firestoreQuery, onSnapshot, addDoc, serverTimestamp} from "firebase/firestore";
-import { useState, useEffect } from 'react';
+import { collection, orderBy, limit, query as firestoreQuery, onSnapshot, addDoc, serverTimestamp } from "firebase/firestore";
+import { useState, useEffect, useRef } from 'react';
 import ChatMessage from './chatmessage';
 import { auth } from './server';
-import firebase from 'firebase/compat/app';
+import SendIcon from '@mui/icons-material/Send';
+import TextField from '@mui/material/TextField';
 
 const messageRef = collection(db, "messages");  // Correctly pass the Firestore instance as the first argument
 const messagesQuery = firestoreQuery(messageRef, orderBy('createdAt'), limit(25));
 
-
-
-
-
-
-
-
 function ChatPage() {
     const [messages, setMessages] = useState([]);
     const [formValue, setFormValue] = useState('');
-
+    const autoscroll = useRef();
 
     const sendMessage = async (e) => {
         e.preventDefault();
 
         const { uid } = auth.currentUser;
 
-        await addDoc(messageRef,{
+        await addDoc(messageRef, {
             text: formValue,
             createdAt: serverTimestamp(),
             uid
         })
         setFormValue('');
+
+        autoscroll.current.scrollIntoView({ behavior: 'smooth' });
     }
     useEffect(() => {
         const unsub = onSnapshot(messagesQuery, (snapshot) => {
@@ -46,9 +42,7 @@ function ChatPage() {
         return () => unsub();
     }, []);
 
-    console.log("In chat page");
     const signOut = useSignOut();
-
     return (
 
 
@@ -62,17 +56,35 @@ function ChatPage() {
                 <Button onClick={signOut} variant="outlined" size="small">Logout</Button>
 
             </Box>
+            <Box>
             {/*messages list*/}
-            <ul style={{ color: "black" }}>
+            <main style={{ color: "black" }}>
                 {messages.map((msg) => (
                     <ChatMessage key={msg.id} message={msg}></ChatMessage>
                 ))}
-            </ul>
+                <div ref={autoscroll}></div>
+            </main>
+
 
             <form onSubmit={sendMessage}>
-                <input value={formValue} onChange={(e) => setFormValue(e.target.value)} />
-                <button type="submit">Send</button>
+                
+            <Box sx={{ display: 'flex', alignItems: 'center', gap: 0 }}> {/* Flex container */}
+                <TextField
+                    size="small"
+                    value={formValue}
+                    onChange={(e) => setFormValue(e.target.value)}
+                />
+                <Button
+                    type="submit"
+                    variant="contained"
+                    size="large"
+                    endIcon={<SendIcon />}
+                >
+                    
+                </Button>
+            </Box>
             </form>
+            </Box>
         </div>
     );
 }
