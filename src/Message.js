f9154130a4793ab573be2c3ba363ba1661bfdf35
Adam Amusa <Adamamusa2004@gmail.com>
2024-10-23 11:51:35 +0100

    variable name change

--- src/Message.js
+++ src/Message.js
@@ -2,29 +2,29 @@ import { useContext } from "react";
 import { ChatContext } from "./ChatContext";
 import { AuthContext } from "./context";
 
-const Message = ({chats}) =>{
+const Message = ({ chats }) => {
 
-    const {currentUser} = useContext(AuthContext);
-    const {data} = useContext(ChatContext)
+    const { currentUser } = useContext(AuthContext);
+    const { data } = useContext(ChatContext)
     const messageClass = data.uid === currentUser.uid ? 'sent' : 'received';
 
 
     if (!chats) {
         return <p>No messages to display.</p>;
     }
-return(
-<div>
-{Object.entries(chats)?.map((chat) => (
-     <div className={`message ${messageClass}`}>
-    <p>{chat.formValue}</p>
-    </div>
-))};
+    return (
+        <div>
+            {Object.entries(chats)?.map((chat) => (
+                <div className={`message ${messageClass}`}>
+                    <p>{chat.message}</p>
+                </div>
+            ))};
 
-</div>
+        </div>
 
 
 
-);
+    );
 }
 
 
