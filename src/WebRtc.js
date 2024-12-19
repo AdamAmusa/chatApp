94ec0704fb0fcf7047345cb297058aeb46762777
Adam Amusa <Adamamusa2004@gmail.com>
2024-12-19 00:41:52 +0000

    updated url

--- src/WebRtc.js
+++ src/WebRtc.js
@@ -11,9 +11,9 @@ const configuration = {
     iceServers: [
         
         {
-            url: 'turn:192.158.29.39:3478?transport=udp',
-            credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-            username: '28224511:1379330808'
+            url: 'turn:openrelay.metered.ca:80',
+            credential: 'openrelayproject',
+            username: 'openrelayproject'
         },
     ]
 };
