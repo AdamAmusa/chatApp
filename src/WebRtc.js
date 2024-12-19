21d03af17e08f5c3b923c6bf0c8333b737c71437
Adam Amusa <Adamamusa2004@gmail.com>
2024-12-19 00:06:59 +0000

    Added more servers

--- src/WebRtc.js
+++ src/WebRtc.js
@@ -9,6 +9,20 @@ import { useMediaStream } from "./MediaStreamContext"; // Ensure correct import
 
 const configuration = {
     iceServers: [
+        "stun:stun.l.google.com:19302",
+        "stun1.l.google.com:19302",
+        "stun2.l.google.com:19302",
+        "stun3.l.google.com:19302",
+        "stun4.l.google.com:19302",
+        "stun.ekiga.net",
+        "stun.ideasip.com",
+        "stun.rixtelecom.se",
+        "stun.schlund.de",
+        "stun.stunprotocol.org:347",
+        "stun.voiparound.com",
+        "stun.voipbuster.com",
+        "stun.voipstunt.com",
+        "stun.voxgratia.org",
         {
             url: 'turn:numb.viagenie.ca',
             credential: 'muazkh',
