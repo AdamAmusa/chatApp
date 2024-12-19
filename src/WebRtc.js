48a9d80b7172edc4f7a2f16edefca4ba11a4d781
Adam Amusa <Adamamusa2004@gmail.com>
2024-12-19 00:21:19 +0000

    updated url

--- src/WebRtc.js
+++ src/WebRtc.js
@@ -9,45 +9,12 @@ import { useMediaStream } from "./MediaStreamContext"; // Ensure correct import
 
 const configuration = {
     iceServers: [
-        {urls: "stun:stun.l.google.com:19302"},
-        {urls:"stun:stun1.l.google.com:19302"},
-        {urls:"stun:stun2.l.google.com:19302"},
-        {urls:"stun:stun3.l.google.com:19302"},
-        {urls: "stun:stun4.l.google.com:19302"},
-        {urls: "stun:stun.ekiga.net"},
-        {urls:"stun:stun.ideasip.com"},
-        {urls:"stun:stun.rixtelecom.se"},
-        {urls:"stun:stun.schlund.de"},
-        {urls:"stun:stun.stunprotocol.org:347"},
-        {urls:"stun:stun.voiparound.com"},
-        {urls:"stun:stun.voipbuster.com"},
-        {urls:"stun:stun.voipstunt.com"},
-        {urls:"stun:stun.voxgratia.org"},
+        
         {
-            urls: 'turn:numb.viagenie.ca',
-            credential: 'muazkh',
-            username: 'webrtc@live.com'
-        },
-        {
-            urls: 'turn:192.158.29.39:3478?transport=udp',
+            url: 'turn:192.158.29.39:3478?transport=udp',
             credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
             username: '28224511:1379330808'
         },
-        {
-            urls: 'turn:192.158.29.39:3478?transport=tcp',
-            credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-            username: '28224511:1379330808'
-        },
-        {
-            urls: 'turn:turn.bistri.com:80',
-            credential: 'homeo',
-            username: 'homeo'
-        },
-        {
-            urls: 'turn:turn.anyfirewall.com:443?transport=tcp',
-            credential: 'webrtc',
-            username: 'webrtc'
-        }
     ]
 };
 export const useCallStatus = () => {
